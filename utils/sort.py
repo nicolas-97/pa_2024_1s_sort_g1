fccf86f0fa6210c2c81e25ebecaef63e587e8346
Samuel Ibanez <samuele.ibanezz@utadeo.edu.co>
2024-02-07 14:56:15 -0500

    Commit

--- utils/sort.py
+++ utils/sort.py
@@ -2,18 +2,23 @@ def shell_sort(vector):
     x = len(vector)
     y = x // 2
     while y > 0:
-        for a in range(y, x):
-            z = vector[a]
-            b = a
-            while b >= y & vector[b - y] >= z:
-                vector[b] = vector[b - y]
-                b - y
-            vector[b] = z
-        y //= 2
+       a = y
+       while a < x:
+          b = a - y
+          while b >= 0:
+            if vector[b + y] > vector [b]:
+                break
+            else:
+                vector[b + y], vector[b] = vector[b], vector[b + y]
+                b= b-y
+            a += 1
+    y = y //2
     return vector 
 
 def quick_sort(vector):
-    return vector
+
+     return vector
+   
 
 def merge_sort(vector):
     return vector
