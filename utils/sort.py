5c9ccf869a9bbc17a6177565dccbd17a3d28c3e3
Andres <papotemalote@yopmail.com>
2024-02-07 14:55:17 -0500

    Primer algoritmo shell

--- utils/sort.py
+++ utils/sort.py
@@ -1,4 +1,16 @@
 def shell_sort(vector):
+    n=len(vector)
+    salto = n//2
+    while salto > 0:
+        for i in range(salto,n):   
+            temp = vector[i]
+            j = i
+            while j >= salto and vector[j - salto] > temp:
+                vector[j] = vector[j - salto]
+                j -= salto
+            vector[j] = temp
+        salto//=2
+
     return vector
 
 def quick_sort(vector):
