cf09db07336b41db64e49a3a2e88cd68de1ad5c0
Juan Camilo Pulido Vargas <juanc.pulidova@utadeo.edu.co>
2024-02-07 15:16:13 -0500

    test

--- utils/sort.py
+++ utils/sort.py
@@ -1,7 +1,32 @@
-def shell_sort(vector):
-    return vector
+def shell_sort(my_lista):
+    num=len(my_lista)//2
+    my_copia=my_lista
+    while(num!=0):
+        num_sub=1
+        while(num_sub<=num):
+            subl=[]
+            obj=num_sub-1
+            while(obj<len(my_lista)):
+                subl.append(my_lista[obj])
+                obj+=num
+            insercion(subl)
+            #print(subl)
+
+            obj1=num_sub-1
+            i=0
+            while(obj1<len(my_lista)):     
+                my_copia[obj1]=subl[i]
+                obj1+=num
+                i+=1
+            
+            num_sub+=1
+        
+        num=num//2
+    return my_lista
 
 def quick_sort(l):
+    if(len(l)==0):
+        return l
     piv=l[0]
     right=[]
     left=[]
@@ -26,17 +51,36 @@ def quick_sort(l):
     
 
 def merge_sort(vector):
-    if len(vector)==1:
-        return vector
+    if len(vector) > 1:
+        mid = len(vector) // 2
+        izquierda = vector[:mid]
+        derecha= vector[mid:]
 
-    mitad=len(vector)//2
-    left=vector[:mitad]
-    right=vector[mitad:]
-    
-    newl=merge_sort(left)
-    newr=merge_sort(right)
+        merge_sort(izquierda)
+        merge_sort(derecha)
+        j=0
+        i=0
+        k=0
+        while i < len(izquierda) and j < len(derecha):
+            if izquierda[i] < derecha[j]:
+                vector[k] = izquierda[i]
+                i += 1
+            else:
+                vector[k] = derecha[j]
+                j += 1
+            k += 1
 
-    return orden(newl,newr)
+        while i < len(izquierda):
+            vector[k] = izquierda[i]
+            i += 1
+            k += 1
+
+        while j < len(derecha):
+            vector[k] = derecha[j]
+            j += 1
+            k += 1
+    return vector
+   
     
 
 
@@ -56,3 +100,14 @@ def orden(newl,newr):
         resultado.append(newr[0])
         newr.pop(0)
     return resultado
+
+def insercion(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]  
+        j = i - 1  
+
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
