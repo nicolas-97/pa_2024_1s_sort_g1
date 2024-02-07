4341083c4d7e1790894b32e64c1e9fc61fdce42a
jvargas <juanp.vargasn@utadeo.edu.co>
2024-02-07 15:53:33 -0500

    'Final_Sort'

--- utils/sort.py
+++ utils/sort.py
@@ -7,8 +7,8 @@ def shell_sort(vector):
             while j >= Longitud and vector[j - Longitud] > valor:
                 vector[j] = vector[j - Longitud]
                 j -= Longitud
-                vector[j] = valor
-                Longitud //= 2
+            vector[j] = valor
+        Longitud //= 2
     return vector
    
 def quick_sort(vector):
@@ -19,9 +19,8 @@ def quick_sort(vector):
         Pivote =vector[0]
         Pivote_izquierdo =[x for x in vector[1:] if x<Pivote]
         Pivote_derecho = [x for x in vector[1:] if x>=Pivote]
-
         return quick_sort(Pivote_izquierdo)+[Pivote]+quick_sort(Pivote_derecho)
-
+    
 def merge_sort(vector):
     if len(vector) > 1:
         Medio = len(vector) // 2
@@ -37,9 +36,9 @@ def merge_sort(vector):
             if Left[i] < Right[j]:
                 vector[k] = Left[i]
                 i += 1
-        else:
-            vector[k] = Right[j]
-            j += 1
+            else:
+                vector[k] = Right[j]
+                j += 1
             k += 1
 
             while i < len(Left):
@@ -47,9 +46,9 @@ def merge_sort(vector):
                 i += 1
                 k += 1
 
-                while j < len(Right):
-                    vector[k] = Right[j]
-                    j += 1
-                    k += 1
+            while j < len(Right):
+                vector[k] = Right[j]
+                j += 1
+                k += 1
 
     return vector
