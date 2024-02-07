43ffb219b0af20203abe72b4ae7a78ebecaf6a24
jvargas <juanp.vargasn@utadeo.edu.co>
2024-02-07 15:07:59 -0500

    'Shell_Quick'

--- utils/sort.py
+++ utils/sort.py
@@ -1,8 +1,27 @@
 def shell_sort(vector):
+    Longitud= len(vector) // 2
+    while Longitud > 0:
+        for i in range(Longitud, len(vector)):
+            valor = vector[i]
+            j = i
+            while j >= Longitud and vector[j - Longitud] > valor:
+                vector[j] = vector[j - Longitud]
+                j -= Longitud
+                vector[j] = valor
+                Longitud //= 2
     return vector
    
 def quick_sort(vector):
-    return vector
+    Longitud =len(vector)
+    if Longitud<=1:
+        return vector
+    else :
+        Pivote =vector[0]
+        Pivote_Izquierdo =[x for x in vector[1:] if x<Pivote]
+        Pivote_Derecho = [x for x in vector[1:] if x>=Pivote]
+
+        return quick_sort(Pivote_Izquierdo)+[Pivote]+quick_sort(Pivote_Derecho)
 
 def merge_sort(vector):
+
     return vector
