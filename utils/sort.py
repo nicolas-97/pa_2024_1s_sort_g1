6903dc8dba004c80076e2e0384f4ef79b9cbd544
Jonatan R <jonatan.riosa@utadeo.edu.co>
2024-02-07 14:54:18 -0500

    TEST 1 PROBADO

--- utils/sort.py
+++ utils/sort.py
@@ -1,4 +1,20 @@
 def shell_sort(vector):
+    largo = 0
+    
+    for i in vector:
+        largo += 1
+    
+    distancia = largo // 2
+    
+    while distancia > 0:
+        for i in range(distancia, largo):
+            val = vector[i]
+            j = i
+            while j >= distancia and vector[j - distancia] > val:
+                vector[j] = vector[j - distancia]
+                j -= distancia
+            vector[j] = val
+        distancia //= 2
     return vector
 
 def quick_sort(vector):
