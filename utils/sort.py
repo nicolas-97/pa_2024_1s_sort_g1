d37aba73c59bdd1bff22fcd5dde6e067cc3ac46c
DuvanPachecoG <oscard.pachecog@utadeo.edu.co>
2024-02-07 15:44:18 -0500

    en el primer ejercicio shell_sort utilizo una variable de espacio para dividir la matriz en subamatricez mas pequeñas y ordenarlas por insercion, la varaible se redice en cada iteracion hasta que se vuelve 1,co esto se organizan los algoritmos sin importar si estan muy separados, en el segundo ejercicio Quick_sort utilizo un metodo (y)para que seleccionen un elemento de la matriz, luego crea tres listas para los elementos menores iguales y mayores y los ordena recursivamente y con esto produce la matriz final, en el tercer ejercicio Merge_sort en este ejercicio la matriz se divide recursivamente en dos mitades hasta que las mitades solo tengan un elemento luego convina las mitades comparando los elementos de cada mitad asi agreagaria el elemnto mas pequeño a el resultado,esto se repite hasta que toda la matriz este en orden

--- utils/sort.py
+++ utils/sort.py
@@ -1,8 +1,54 @@
+
+from math import pi
+from tkinter import Y
+
+
 def shell_sort(vector):
+    x=len(vector)//2
+    while x>0:
+        for i in range(x,len(vector)):
+            abc=vector[i]
+            y=i
+            while y>=x and vector[y-x]>abc:
+                vector[y]=vector[y-x]
+                y-=x
+            vector[y]=abc
+        x//=2
     return vector
 
 def quick_sort(vector):
+    if len(vector)<=1:
+        return(vector)
+
+    y=vector[len(vector)//2]
+    izq=[x for x in vector if x<y]
+    mid= [x for x in vector if x ==y]
+    der=[x for x in vector if x>y]
+    resultado=(quick_sort(izq)+mid+quick_sort(der))
+    vector=resultado
     return vector
 
 def merge_sort(vector):
+    if len(vector)<=1:
+        return vector
+    mid= len(vector)//2
+    izq= vector[:mid]
+    der=vector[mid:]
+    izq = merge_sort(izq)
+    der= merge_sort(der)
+    return merge(izq,der)
+
+def merge(izq,der):
+    vector=[]
+    i=y=0
+    while i < len(izq)and y < len(der):
+        if izq[i]<der[y]:
+            vector.append(izq[i])
+            i+=1
+        else:
+            vector.append(der[y])
+            y+=1
+    vector+= izq[i:]
+    vector+= der[y:]
+        
     return vector
