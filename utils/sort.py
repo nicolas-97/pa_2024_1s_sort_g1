4009892601cbbff04082ac7f0da1534ebb75c922
Jlezama <juanp.lezamac@utade.edu.co>
2024-02-07 14:47:48 -0500

    primera prueba

--- utils/sort.py
+++ utils/sort.py
@@ -1,4 +1,21 @@
 def shell_sort(vector):
+    size=len(vector)
+    interval = size//2
+
+    while interval > 0:
+
+        for j in range(interval,size):
+            
+            actual=vector[j]
+            index_actual = j
+            
+            compare_value = j-interval
+            while index_actual >= interval and vector[compare_value] > actual:
+                vector[j]=vector[compare_value]
+                index_actual -= interval
+            vector[compare_value]=actual
+        interval //=2
+    
     return vector
 
 def quick_sort(vector):
