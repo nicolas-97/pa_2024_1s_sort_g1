c9de593f5f9c102dc4cdb32d51c18731f0c8a102
Juand2021 <158349043+Juand2021@users.noreply.github.com>
2024-02-07 16:02:47 -0500

    Update sort.py

--- utils/sort.py
+++ utils/sort.py
@@ -1,5 +1,20 @@
-def shell_sort(vector):
-    return vector
+def shell_sort(lista):
+    grupo = len(lista) // 2
+    
+    while grupo > 0:
+        for i in range(grupo, len(lista)):
+            temp = lista[i]
+            j = i
+            
+            while j >= grupo and lista[j - grupo] > temp:
+                lista[j] = lista[j - grupo]
+                j -= grupo
+            
+            lista[j] = temp
+        
+        grupo //= 2
+    
+    return lista
 
 def quick_sort(vector):
     return vector
