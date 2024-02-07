492cfdfe8b92e3511cd8b5ed294fb6e1435af8c2
unknown <usuario1@M26-S5-44-10664.utadeo.loc>
2024-02-07 14:22:20 -0500

    Prueba de shell

--- utils/sort.py
+++ utils/sort.py
@@ -1,5 +1,25 @@
+
 def shell_sort(vector):
+    size=len(vector)
+    distancia=size//2
+    temp=0
+
+    while True:
+        for i in range(distancia):
+            temp=i
+            while distancia+i<size:
+                if vector[distancia+i]<vector[temp]:
+                    vector[distancia+i], vector[temp] = vector[temp], vector[distancia+i]
+                    temp=distancia+i
+                i+=distancia
+                
+        if distancia==1:
+            break
+        distancia//=2
+        
     return vector
+
+
 
 def quick_sort(vector):
     return vector
