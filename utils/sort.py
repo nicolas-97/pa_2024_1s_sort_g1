9b63f52015ff21e4804d8372f07d157a01538c5c
sara <usuario1@S26-S5-22-10665.utadeo.loc>
2024-02-07 13:47:32 -0500

    Ya funciona todo, subanle la dificultad a los bots

--- utils/sort.py
+++ utils/sort.py
@@ -1,8 +1,51 @@
 def shell_sort(vector):
+    n = len(vector)
+    gap = n // 2
+    while gap > 0:
+        j = gap
+        while j < n:
+            i = j - gap
+            while i >= 0:
+                if vector[i + gap] > vector [i]:
+                    break
+                else:
+                    vector[i + gap], vector[i] = vector[i], vector[i + gap]
+                i = i - gap
+            j += 1
+        gap = gap//2
     return vector
 
 def quick_sort(vector):
-    return vector
+    if len(vector) <= 1:
+        return vector
+    pivot = vector[len(vector) // 2]
+    left = [x for x in vector if x < pivot]
+    middle = [x for x in vector if x == pivot]
+    right = [x for x in vector if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
 
 def merge_sort(vector):
+    if len(vector) > 1:
+        mid = len(vector) // 2
+        L = vector[:mid]
+        R = vector[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                vector[k] = L[i]
+                i += 1
+            else:
+                vector[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            vector[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            vector[k] = R[j]
+            j += 1
+            k += 1
     return vector
