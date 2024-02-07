3c7e2b9f6a4cd361334054059b611d1612976219
Sebas San <juans.sanchezro@utado.edu.co>
2024-02-07 14:14:57 -0500

    'shell_sort'

--- utils/sort.py
+++ utils/sort.py
@@ -1,4 +1,15 @@
 def shell_sort(vector):
+    lenght= len(vector)
+    interval= lenght//2
+    while interval > 0:
+        for i in range(interval, lenght):
+            insert_value = vector[i]
+            insert_index=i
+        while insert_index >= interval and vector[insert_index - interval]>insert_value:
+            vector[insert_index]=vector[insert_index-interval]
+            insert_index -= interval
+            vector[insert_index]= insert_value
+        interval//2
     return vector
 
 def quick_sort(vector):
