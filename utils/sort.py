13a108bf024a77f484d5a695ebd9038689a13cd3
Sebastian Romero <sebastiand.romeroc@utadeo.edu.co>
2024-02-09 23:43:18 -0500

    Entrega_final

--- utils/sort.py
+++ utils/sort.py
@@ -1,4 +1,3 @@
-
 def shell_sort(vector):
     mitad = len(vector) // 2
     
@@ -19,12 +18,68 @@ def opitimizar_busqueda (lista, inicio, salto):
         lista[posicion] = valor  
 
 def quick_sort(vector):
+    quick_sort_auxiliar(vector, 0, len(vector) - 1)
     return vector
+
+def quick_sort_auxiliar(lista, inicio, fin):
+    if inicio < fin:
+        punto_particion = particionar(lista, inicio, fin)
+
+        quick_sort_auxiliar(lista, inicio, punto_particion - 1)
+        quick_sort_auxiliar(lista, punto_particion + 1, fin)
+
+def particionar(lista, inicio, fin):
+    pivote = lista[inicio]
+
+    izquierda = inicio + 1
+    derecha = fin
+    terminado = False
+
+    while not terminado:
+        while izquierda <= derecha and lista[izquierda] <= pivote:
+            izquierda += 1
+        
+        while lista[derecha] >= pivote and derecha >= izquierda:
+            derecha -= 1
+        
+        if derecha < izquierda:
+            terminado = True
+        else:
+            lista[izquierda], lista[derecha] = lista[derecha], lista[izquierda]
+    
+    lista[inicio], lista[derecha] = lista[derecha], lista[inicio]
+
+    return derecha
+
 
 def merge_sort(vector):
-    return vector
+    if len(vector) > 1:
+        mitad = len(vector) // 2
+        primera_mitad = vector[:mitad]
+        segunda_mitad = vector[mitad:]
 
-numeros = [7,2,11,3,1,13,5]
-print (numeros)
-shell_sort(numeros)
-print(f"Lista organizada {numeros}")
+        merge_sort(primera_mitad)
+        merge_sort(segunda_mitad)
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(primera_mitad) and j < len(segunda_mitad):
+            if primera_mitad[i] < segunda_mitad[j]:
+                vector[k] = primera_mitad[i]
+                i += 1
+            else:
+                vector[k] = segunda_mitad[j]
+                j += 1
+            k += 1
+        
+        while i < len(primera_mitad):
+            vector[k] = primera_mitad[i]
+            i += 1
+            k += 1
+        
+        while j < len(segunda_mitad):
+            vector[k] = segunda_mitad[j]
+            j += 1
+            k += 1
+    return vector
