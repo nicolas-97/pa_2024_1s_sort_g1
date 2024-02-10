6fb710c72ae1b6195d8534cf15e855b6695e0760
Maximiliano Valeta <maximiliano.valeta@ecopetrol.com.co>
2024-02-09 22:23:18 -0500

    Primer ejercicio

--- utils/sort.py
+++ utils/sort.py
@@ -1,5 +1,21 @@
+def opitimizar_busqueda (lista, inicio, salto):
+    for i in range (inicio + salto, len(lista, salto)):
+        valor = lista [i]
+        posicion = i
+
+        while posicion >= salto and lista [posicion - salto] >valor:
+            lista[posicion] = lista[posicion - salto]
+            posicion = posicion - salto
+        lista[posicion] = valor  
+
 def shell_sort(vector):
-    return vector
+    mitad = len(vector) // 2
+
+    while mitad > 0:
+        for i in range(mitad):
+            opitimizar_busqueda(vector, i, mitad)
+        mitad = mitad // 2
+    return vector 
 
 def quick_sort(vector):
     return vector
