663cb1ed72c0080409723ce9f5ceb8b4bd6108c2
Juan Sebastian Sanchez Rodriguez <juans.sanchezro@utadeo.edu.co>
2024-02-09 19:40:44 -0500

    ahora si

--- utils/sort.py
+++ utils/sort.py
@@ -13,27 +13,48 @@ def shell_sort(vector):
     return vector
 
 def quick_sort(vector):
-   def quicksort(vector,left,right):
-        partition_pos=partition(vector,left,right)
-        quicksort(vector,left,partition_pos -1)
-        quicksort(vector,partition_pos+1, right)
-   
-def partition(vector,left,right):  
-    i=left
-    j=right+1
-    pivot= vector[right]
-    while i<j:
-        while i < right and vector[i] <pivot:
-            i+=1
-        while j > left and vector[j] >=pivot:
-            j-=1
-        if i<j:
-            vector[i], vector[j] = vector[j], vector[i]    
-    if vector[i] > pivot:
-        vector[i], vector[right] = vector[right], vector[i]
-    return vector
+    if len(vector) <= 1:
+        return vector
+    pivot = vector[len(vector) // 2]
+    left = [x for x in vector if x < pivot]
+    middle = [x for x in vector if x == pivot]
+    right = [x for x in vector if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
 
 def merge_sort(vector):
-    return vector
+    if len(vector) <= 1:
+        return vector
+   
+    list_1 = vector[0:len(vector) // 2]
+    list_2 = vector[len(vector) // 2:]
+    
+    ans_1 = merge_sort(list_1)
+    ans_2 = merge_sort(list_2)
+    
+    sort_list = sort_two_list(ans_1, ans_2)
+    return sort_list
 
-print('dejame hcer algoooo')
+def sort_two_list(list_1, list_2):
+    final_list = []
+    i = 0
+    j = 0
+    while i < len(list_1) and j < len(list_2):
+        if list_1[i] <= list_2[j]:
+            final_list.append(list_1[i])
+            i += 1
+            continue
+        final_list.append(list_2[j])
+        j += 1
+
+    while i < len(list_1):
+        final_list.append(list_1[i])
+        i = i + 1
+        
+    while j < len(list_2):
+        final_list.append(list_2[j])
+        j = j + 1
+        
+    return final_list
+  
+    
+    
