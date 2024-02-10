d0974341fc034ed5dcd51c0bf171326195f37b80
Mariana0102 <158349418+Mariana0102@users.noreply.github.com>
2024-02-09 22:03:43 -0500

    agregar tres algoritmos de ordenamiento

--- utils/sort.py
+++ utils/sort.py
@@ -1,8 +1,23 @@
 def shell_sort(vector):
+    n = len(vector)
+    brecha = n // 2
+
+    while brecha > 0:
+        for i in range(brecha, n):
+            temp = vector [i]
+            j = i
+            while j >= brecha and vector[j - brecha] > temp:
+                vector [j] = vector[j - brecha]
+                j -= brecha
+            vector [j] = temp
+        brecha //= 2
+
     return vector
 
-def quick_sort(vector):
-    return vector
+
+
+
 
 def merge_sort(vector):
+    
     return vector
