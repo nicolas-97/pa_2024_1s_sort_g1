4061852c5a4c2b252a2e50a6edbd9596ad7d22ee
NataliaOsejo <nataliaosejounv@gmail.com>
2024-02-11 08:38:59 -0500

    prueba casa

--- utils/sort.py
+++ utils/sort.py
@@ -33,36 +33,3 @@ m = shell_sort(vect)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def ordenamiento_insercion(vector,longitud_vector): #Ordenamiento que compara los elementos por parejas y los reubica a la posición favorable. 
-    #Este ordenamiento en la comparación tambien revisa si el antecesor del elemento que estamos comparando es menor y de ser asi intercambia posiciones.
-    for i in range(longitud_vector-1): #Posicion del elemento con el cual empezamos a comparar
-        for j in range(i+1, longitud_vector): #Posicion del elemento con el cual comparamos i 
-            if vector[i] > vector[j]:
-                vector[i], vector[j] = vector[j], vector[i]
-                if i != 0: #Vamos a comparar si el elemento en i es menor a elementos anteriores
-                    dato = vector[i]
-                    for veces in range(len(vector[:i])):
-                        n = vector.index(dato) #n = Posicion actual del dato que antes estaba en i.
-                        if vector[n-1] > vector[n]:
-                            vector[n-1], vector[n] = vector[n], vector[n-1]
-                        else:
-                            continue
-            i += 1
-    return print(f'vector ordenada : {vector}')
