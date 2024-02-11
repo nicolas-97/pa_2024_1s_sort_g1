810d6ae8a70cca381048248bafc08c13a076e593
NataliaOsejo <nataliaosejounv@gmail.com>
2024-02-11 14:49:41 -0500

    Merge sort 'listo'

--- utils/sort.py
+++ utils/sort.py
@@ -21,12 +21,42 @@ def quick_sort(vector):
     return vector
 
 
-
 def merge_sort(vector):
-    return vector
+    if len(vector) == 1: #caso base
+        return vector
+    
+    longitud = len(vector)//2
+    vector_der = vector[longitud:] #se crean dos subgrupos
+    vector_iz = vector[:longitud]
 
-vect = [10,5,48,12,36,51,1]
-m = shell_sort(vect)
+    vector_der_orden = merge_sort(vector_der) #recursividad para llevar a todos los subgrupos al caso base
+    vector_iz_orden = merge_sort(vector_iz)
+
+    return merge(vector_der_orden, vector_iz_orden) #se retorna el array ordenado
+
+def merge(vector_der, vector_iz):
+    vector_ordenado = []
+    while len(vector_der) > 0 and len(vector_iz) > 0: #Ver que no esten vacios
+        if vector_der[0] > vector_iz[0]: #añadir al vector final el valor más bajo
+            vector_ordenado.append(vector_iz[0])
+            vector_iz.pop(0) #Se elimina del array el vector añadido al resultado
+        else:
+            vector_ordenado.append(vector_der[0])
+            vector_der.pop(0)
+    
+    #El último y más grande valor se agrega
+    while len(vector_der) > 0:
+        vector_ordenado.append(vector_der[0])
+        vector_der.pop(0)
+
+    while len(vector_iz) > 0:
+        vector_ordenado.append(vector_iz[0])
+        vector_iz.pop(0)
+
+    return vector_ordenado
+
+vect = [30,21,8,58,1,5]
+m = merge_sort(vect)
 print(m)
 
 
