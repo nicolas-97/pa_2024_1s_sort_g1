03326465ab62a7a07378c8cecde06b5d108bb65d
mafemoreno25 <159285330+mafemoreno25@users.noreply.github.com>
2024-02-13 19:25:46 -0500

    Add files via upload

--- sort.py
+++ sort.py
@@ -1,74 +1,65 @@
 def shell_sort(vector):
-    n = len(vector)
-    brecha = n // 2
+    gap = len(vector) // 2 
 
-    while brecha > 0:
-        for i in range(brecha, n):
-            temp = vector [i]
-            j = i
-            while j >= brecha and vector[j - brecha] > temp:
-                vector [j] = vector[j - brecha]
-                j -= brecha
-            vector [j] = temp
-        brecha //= 2
+    while gap > 0: 
+        for i in range(gap, len(vector)): 
+            temp = vector[i] 
+
+            j = i 
+
+            while j >= gap and vector[j - gap] > temp: 
+
+                vector[j] = vector[j - gap] 
+
+                j -= gap 
+
+            vector[j] = temp 
+
+
+        gap //= 2 
 
     return vector
-
-import time
-
-#
 
 def quick_sort(vector):
- 
-    if len(vector) <= 1:
+    return vector
+    if len (vector)<=1:
         return vector
 
-    pivot = vector[len(vector) // 2]
-    left = [x for x in vector if x < pivot]
-    middle = [x for x in vector if x == pivot]
-    right = [x for x in vector if x > pivot]
+    else:
+        pivote=vector[0]
 
-    return quick_sort(left) + middle + quick_sort(right)
-
-vector = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-sorted_vector = quick_sort(vector)
-print(sorted_vector)
-
-
-strat_time=time.time()
-quick_sort(vector)
-end_time=time.time()
+        izq=[x for x in vector[1:] if x<pivote]
+        der=[x for x in vector[1:] if x>=pivote]
+        return quick_sort(izq)+[pivote]+quick_sort(der)
 
 def merge_sort(vector):
-    if len(vector) <= 1:
-        return vector
-    
-    izquierda = merge_sort(vector[:len(vector)//2])
-    derecha = merge_sort(vector[len(vector)//2:])
-    
-    i=0 # L index
-    j=0 # R index
-    k=0 # index vector sorted
+    if len(vector) > 1:
+        mid = len(vector) // 2
+        izq = vector[:mid]
+        der = vector[mid:]
 
-    while i < len(izquierda) and j < len(derecha):
-        if izquierda[i] < derecha[j]:
-            vector[k] = izquierda[i]
+        merge_sort(izq)
+        merge_sort(der)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(izq) and j < len(der):
+            if izq[i] < der[j]:
+                vector[k] = izq[i]
+                i += 1
+            else:
+                vector[k] = der[j]
+                j += 1
+            k += 1
+
+        while i < len(izq):
+            vector[k] = izq[i]
             i += 1
-        else:
-            vector[k] = derecha[j]
+            k += 1
+
+        while j < len(der):
+            vector[k] = der[j]
             j += 1
-        k += 1
-
-    while i < len(izquierda):
-        vector[k] = izquierda[i]
-        i += 1
-        k += 1
-
-    while j < len(derecha):
-        vector[k] = derecha[j]
-        j += 1
-        k += 1
+            k += 1
 
     return vector
-
-
