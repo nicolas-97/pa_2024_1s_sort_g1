2de1e792a0663b1aa8a2c4bda650abfc02c48e50
locopower111 <117127479+locopower111@users.noreply.github.com>
2024-02-21 23:35:32 -0500

    solucion

---
+++ test/utils/sort.py
@@ -0,0 +1,44 @@
+def shell_sort(vector):
+    n = len(vector)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = vector[i]
+            j = i
+            while j >= gap and vector[j - gap] > temp:
+                vector[j] = vector[j - gap]
+                j -= gap
+            vector[j] = temp
+        gap //= 2
+    return vector
+
+def quick_sort(vector):
+    if len(vector) <= 1:
+        return vector
+    pivot = vector[len(vector) // 2]
+    left = [x for x in vector if x < pivot]
+    middle = [x for x in vector if x == pivot]
+    right = [x for x in vector if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+def merge_sort(vector):
+    if len(vector) <= 1:
+        return vector
+    middle = len(vector) // 2
+    left = merge_sort(vector[:middle])
+    right = merge_sort(vector[middle:])
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+    return result
