56b850b15043c15a04b7518dff5609e5f1f28b18
Andres Alaix <80136164+alaixgg@users.noreply.github.com>
2024-02-24 04:28:48 -0500

    Update sort.py
    Full :,)

--- utils/sort.py
+++ utils/sort.py
@@ -1,84 +1,65 @@
 def shell_sort(vector):
     size = len(vector)
-    subsize = size // 2
-    
-    while subsize > 0:
-        for i in range(subsize, size):
+    gap = size // 2
+
+    while gap > 0:
+        for i in range(gap, size):
             temp = vector[i]
             j = i
-            while j >= subsize and vector[j - subsize] > temp:
-                vector[j] = vector[j - subsize]
-                j -= subsize
+
+            while j >= gap and vector[j - gap] > temp:
+                vector[j] = vector[j - gap]
+                j -= gap
+
             vector[j] = temp
-        subsize //= 2
+
+        gap //= 2
 
     return vector
+
 
 def quick_sort(vector):
-
-    size = len(vector)
-    prepiv = size // 2
-    piv = vector[prepiv]
-    i = 0
-    j = 0
-    z= size -1
-    Condi1 = False  
-    Condi2 = False 
-    arreglo=[0,1]
-    prepiv2=prepiv
-    for i in range(1, prepiv2 + 1):
-        while prepiv > i: 
-            
-            if vector[j] > piv:
-                arreglo[0]=vector[j]
-                Condi1 = True
-                
-            if vector[z] < piv:
-                arreglo[1]=vector[-1]
-                Condi2 = True
-            if Condi1 and Condi2:
-             vector[z], vector[j] = arreglo[0],arreglo[1]
-            Condi1 = False  
-            Condi2 = False 
-            j += 1
-            z -= 1
-            i += 1
-    prepiv2 -=1
-    prepiv = prepiv//2
-    return vector
-       
-
-
-def merge_sort(arr):
-     if len(arr) > 1:
-        mid = len(arr) // 2 
-        left_half = arr[:mid]  
-        right_half = arr[mid:]
-
-        merge_sort(left_half) 
-        merge_sort(right_half) 
-
+        if len(vector) <= 1 :
+            return vector
         
-        i = j = k = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
+        pivot = len(vector) // 2 
+        equal = [x for x in vector if x == vector[pivot]]
+        left = [x for x in vector if x < vector [pivot]]
+        right = [x for x in vector if x > vector[pivot]]
 
-       
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
+        return quick_sort(left) + equal  +quick_sort(right)
+     
+     
 
+def merge_sort(vector):    
+    if len(vector) == 1:
+        return vector
+    else:
+        mid = len(vector) // 2
+        left = vector[:mid]
+        right = vector[mid:]
         
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
+        left_sorted = merge_sort(left)
+        right_sorted = merge_sort(right)
+        
+        return merge_sort2(left_sorted, right_sorted)
 
-     return vector
+def merge_sort2(left, right):
+    vector_arreglado = []
+    while len(left) > 0 and len(right) > 0:
+        if left[0] > right[0]:
+            vector_arreglado.append(right[0])
+            right.pop(0)
+        else:
+            vector_arreglado.append(left[0])
+            left.pop(0)
+                     
+    while len(left) > 0:
+        vector_arreglado.append(left[0])
+        left.pop(0)
+        
+    while len(right) > 0:
+        vector_arreglado.append(right[0])
+        right.pop(0)
+    
+    return vector_arreglado
